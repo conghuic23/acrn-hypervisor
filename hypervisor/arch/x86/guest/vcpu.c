529e1a88182df61379eb06bd48da22dbc0d73286
Shuo A Liu <shuo.a.liu@intel.com>
2019-06-14 13:10:05 +0800

    hv: sched: add sleep/wake for schedule object
    sleep one sched_object means to prevent it from being scheduled.
    wake one sched_object is an opposite operation of sleep.
    This patch also add notify_mode in sched_object to indicate how to
    deliver the request.
    
    Signed-off-by: Jason Chen CJ <jason.cj.chen@intel.com>
    Signed-off-by: Yu Wang <yu1.wang@intel.com>
    Signed-off-by: Shuo A Liu <shuo.a.liu@intel.com>

--- hypervisor/arch/x86/guest/vcpu.c
+++ hypervisor/arch/x86/guest/vcpu.c
@@ -674,46 +674,25 @@ void pause_vcpu(struct acrn_vcpu *vcpu, enum vcpu_state new_state)
 	pr_dbg("vcpu%hu paused, new state: %d",
 		vcpu->vcpu_id, new_state);
 
-	get_schedule_lock(pcpu_id);
 	vcpu->prev_state = vcpu->state;
 	vcpu->state = new_state;
-
-	if (vcpu->running) {
-		sched_remove(&vcpu->sched_obj, pcpu_id);
-
-		if (is_lapic_pt_enabled(vcpu)) {
-			make_reschedule_request(pcpu_id, DEL_MODE_INIT);
-		} else {
-			make_reschedule_request(pcpu_id, DEL_MODE_IPI);
+	sched_sleep_obj(&vcpu->sched_obj);
+	if (pcpu_id != get_pcpu_id()) {
+		while (vcpu->running) {
+			asm_pause();
 		}
-
-		release_schedule_lock(pcpu_id);
-
-		if (pcpu_id != get_pcpu_id()) {
-			while (vcpu->running) {
-				asm_pause();
-			}
-		}
-	} else {
-		sched_remove(&vcpu->sched_obj, pcpu_id);
-		release_schedule_lock(pcpu_id);
 	}
 }
 
 void resume_vcpu(struct acrn_vcpu *vcpu)
 {
-	uint16_t pcpu_id = pcpuid_from_vcpu(vcpu);
-
 	pr_dbg("vcpu%hu resumed", vcpu->vcpu_id);
 
-	get_schedule_lock(pcpu_id);
 	vcpu->state = vcpu->prev_state;
 
 	if (vcpu->state == VCPU_RUNNING) {
-		sched_insert(&vcpu->sched_obj, pcpu_id);
-		make_reschedule_request(pcpu_id, DEL_MODE_IPI);
+		sched_wake_obj(&vcpu->sched_obj);
 	}
-	release_schedule_lock(pcpu_id);
 }
 
 static void context_switch_out(struct sched_object *prev)
@@ -792,6 +771,8 @@ int32_t prepare_vcpu(struct acrn_vm *vm, uint16_t pcpu_id)
 		vcpu->sched_obj.ctx = &per_cpu(sched_ctx, pcpu_id);
 		vcpu->sched_obj.thread = vcpu_thread;
 		vcpu->sched_obj.pcpu_id = pcpu_id;
+		vcpu->sched_obj.notify_mode = is_lapic_pt_enabled(vcpu) ?
+			SCHED_NOTIFY_INIT : SCHED_NOTIFY_IPI;
 		vcpu->sched_obj.host_sp = build_stack_frame(vcpu);
 		vcpu->sched_obj.switch_out = context_switch_out;
 		vcpu->sched_obj.switch_in = context_switch_in;

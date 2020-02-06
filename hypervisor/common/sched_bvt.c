1e6376626417b43ecd278f22c4a22d9c8a14d265
Conghui Chen <conghui.chen@intel.com>
2020-02-06 16:02:10 +0000

    hv: sched_bvt: add init and deinit function
    Add init function for bvt scheduler, creating a runqueue and a period
    timer, the timer interval is default as 1ms. The interval is the minimum
    charging unit.
    Add structure sched_bvt_data, and init some members in it.
    
    Signed-off-by: Conghui Chen <conghui.chen@intel.com>

--- hypervisor/common/sched_bvt.c
+++ hypervisor/common/sched_bvt.c
@@ -31,17 +31,50 @@ struct sched_bvt_data {
 	uint64_t start_tsc;
 };
 
-int sched_bvt_init(__unused struct sched_control *ctl)
-{
-	return 0;
-}
-
-void sched_bvt_deinit(__unused struct sched_control *ctl)
+static void sched_tick_handler(__unused void *param)
 {
 }
 
-void sched_bvt_init_data(__unused struct thread_object *obj)
+int sched_bvt_init(struct sched_control *ctl)
 {
+	struct sched_bvt_control *bvt_ctl = &per_cpu(sched_bvt_ctl, ctl->pcpu_id);
+	uint64_t tick_period = CONFIG_MCU_MS * CYCLES_PER_MS;
+	int ret = 0;
+
+	ASSERT(get_pcpu_id() == ctl->pcpu_id, "Init scheduler on wrong CPU!");
+
+	ctl->priv = bvt_ctl;
+	INIT_LIST_HEAD(&bvt_ctl->runqueue);
+
+	/* The tick_timer is periodically */
+	initialize_timer(&bvt_ctl->tick_timer, sched_tick_handler, ctl,
+			rdtsc() + tick_period, TICK_MODE_PERIODIC, tick_period);
+
+	if (add_timer(&bvt_ctl->tick_timer) < 0) {
+		pr_err("Failed to add schedule tick timer!");
+		ret = -1;
+	}
+
+	return ret;
+}
+
+void sched_bvt_deinit(struct sched_control *ctl)
+{
+	struct sched_bvt_control *bvt_ctl = (struct sched_bvt_control *)ctl->priv;
+	del_timer(&bvt_ctl->tick_timer);
+}
+
+void sched_bvt_init_data(struct thread_object *obj)
+{
+	struct sched_bvt_data *data;
+
+	data = (struct sched_bvt_data *)obj->data;
+	INIT_LIST_HEAD(&data->list);
+	data->mcu = CONFIG_MCU_MS * CYCLES_PER_MS;
+	/* TODO: mcu advance value should be proportional to weight. */
+	data->vt_ratio = 1;
+	data->cs_allow = CONFIG_CSA_MCU_NUM;
+	data->run_countdown = data->cs_allow;
 }
 
 static struct thread_object *sched_bvt_pick_next(__unused struct sched_control *ctl)
